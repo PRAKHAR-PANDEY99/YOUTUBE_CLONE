7327ce68ebfd25594182285b50872ae5a37fa734
Prakhar Pandey <prakharpandey1309@gmail.com>
2025-11-03 14:30:43 +0530

    REMOVED UNUSED FILES

--- html - Copy/py.py
+++
@@ -1,2 +0,0 @@
-for i in  range (1,56,3):
-    print(i)
--- python/FIRSTSTEP.java
+++
@@ -1,6 +0,0 @@
-public class FIRSTSTEP{
-    public static void main(String[] args){
-        System.out.println("Hello World");
-    }
-}
-    
